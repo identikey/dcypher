721a352d138138b43a32f1067c89f0df8253c9cf
claysauruswrecks <44852834+claysauruswrecks@users.noreply.github.com>
2025-06-22 19:47:42 -0700

    split up auth_oqs tests

---
+++ tests/unit/test_auth_oqs_valid.py
@@ -0,0 +1,17 @@
+import oqs
+import pytest
+from lib.pq_auth import verify_pq_signature
+
+from .util.util import get_enabled_sigs
+
+
+@pytest.mark.parametrize("alg", get_enabled_sigs())
+def test_verify_pq_signature_valid(alg: str) -> None:
+    """
+    Tests that a valid post-quantum signature is correctly verified.
+    """
+    message = b"This is a test message."
+    with oqs.Signature(alg) as sig:
+        public_key = sig.generate_keypair()
+        signature = sig.sign(message)
+        assert verify_pq_signature(public_key.hex(), signature.hex(), message, alg)
